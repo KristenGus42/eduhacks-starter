5d46577068a8c8a8ab01358a6d77659cb83194f7
klg42 <klg42@uw.edu>
2024-01-04 10:10:48 -0800

    Initial changes - what it should always look like

---
+++ app.js
@@ -0,0 +1,32 @@
+// Sets up node server
+import express from 'express';
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// TODO: Import OpenAI and enter your openAI key
+const KEY = "YOUR KEY";
+import OpenAI from "openai";
+const openai = new OpenAI({apiKey: KEY});
+
+//TODO: Create constants openai and messages
+
+/**
+ * Gets reponse from OpenAI servers
+ * @param {String} query user input
+ */
+export async function chat(query) {
+  // TODO: Create code that compiles past responses and generates a new one by accessing information from OpenAI
+}
+
+/**
+ * A GET endpoint that will send output to a client
+ */
+app.get('/response/:message', async (req, res) => {
+  // TODO: Create a get endpoint that sends openAI output to client
+})
+
+// Set up port and define proper files
+app.use(express.static('scripts'));
+app.listen(3000)
---
+++ scripts/openai-test.js
@@ -0,0 +1,47 @@
+'use strict';
+(function() {
+
+  window.addEventListener("load", init);
+
+  /**
+   * Sets up interactive portion of the window
+   */
+  function init() {
+    //TODO: Set up interactivity of website
+  }
+
+  /**
+   * Displays the user's input on the screen
+   */
+  function displayUser() {
+    // TODO: Write the code to display user information
+  }
+
+  /**
+   * Requests a response from our own backend
+   */
+  async function fetchResponse() {
+    // TODO: Write the code to fetch information from node server
+  }
+
+  /**
+   * Displays the bot's response on the screen
+   * @param {Promise} res information from OpenAI servers
+   */
+  function displayBot(res) {
+    // TODO: Display bot's response
+  }
+
+  /**
+   * Checks the status of the response recieved
+   * @param {Response} res given response to check
+   * @returns {Response} indicating the status of the given response
+   */
+  async function statusCheck(res) {
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return res;
+  }
+
+})();
